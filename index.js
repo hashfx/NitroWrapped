8dc102f66eb22e4f8c34687044a9317ac145bdb8
Harsh Soni <algebra.soni@gmail.com>
2024-12-12 11:47:44 +0000

    config: change PORT to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3100;
+const PORT = 3000;
 
 const NITRO_API_URL = 'https://api.explorer.routernitro.com/graphql';
 
